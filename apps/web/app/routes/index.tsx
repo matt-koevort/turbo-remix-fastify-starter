bb6896838cd0904220b1e98736f3963383e0b9fa
Matt Koevort <matt@plusplusminus.co.za>
2022-02-20 13:01:32 +0200

    chore: allow importing from ui package in remix web app

--- apps/web/app/routes/index.tsx
+++ apps/web/app/routes/index.tsx
@@ -1,10 +1,11 @@
 import { json, LoaderFunction, useLoaderData } from "remix";
+import { Button } from "ui";
 
 export const loader: LoaderFunction = async () => {
-  const res = await fetch('http://localhost:8080/');
+  const res = await fetch("http://localhost:8080/");
   const data = await res.text();
-  return json(data)
-}
+  return json(data);
+};
 
 export default function Index() {
   const data = useLoaderData();
@@ -12,7 +13,8 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-     <div>{data}</div>
+      <div>{data}</div>
+      <Button />
     </div>
   );
 }
